877a4d617bb32c8195aff2db74fac6b0bb447758
m-allanson <michael.allanson@gmail.com>
2021-03-30 15:59:29 +0100

    Add tests and write module

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use color_eyre::eyre::Result;
+use directories::UserDirs;
+use color_eyre::eyre::{eyre, Result, WrapErr};
+use digital_garden::write;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 /// A CLI for the growing and curation of a digital garden
@@ -7,6 +10,14 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "dg")]
 struct Opt {
+    #[structopt(
+        parse(from_os_str),
+        short = "p",
+        long,
+        env
+    )]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -25,9 +36,23 @@ enum Command {
     },
 }
 
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new().ok_or_else(|| {
+        eyre!("Could not find home directory")
+    })?;
+    Ok(user_dirs.home_dir().join(".garden"))
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
-    dbg!(opt);
-    todo!();
+    dbg!(&opt);
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_garden_dir().wrap_err("`garden_path` was not supplied"),
+
+    }?;
+    match opt.cmd {
+        Command::Write { title } => write(garden_path, title),
+    }
 }
---
+++ tests/integration.rs
@@ -0,0 +1,30 @@
+use assert_cmd::Command;
+use color_eyre::eyre::Result;
+
+#[test]
+// make sure help runs. This indicates the binary works
+fn test_help() -> Result<()>{
+    let mut cmd = Command::cargo_bin("dg").unwrap();
+    let assert = cmd.arg("--help").assert();
+    assert.success().stderr("");
+    Ok(())
+}
+
+#[test]
+/// make sure we have a write command by running 'garden write --help'
+fn test_write_help() -> Result<()>{
+    let mut cmd = Command::cargo_bin("dg")?;
+    let assert = cmd.arg("write").arg("--help").assert();
+    assert.success().stderr("");
+    Ok(())
+}
+
+
+#[test]
+#[ignore]
+/// execute the write command, saving a file out
+fn test_write() {
+    let mut cmd = Command::cargo_bin("dg").unwrap();
+    let assert = cmd.arg("write").assert();
+    assert.success();
+}
