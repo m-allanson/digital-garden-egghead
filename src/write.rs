48f788e87290dc6571aefe6fc5b98c4b2e40f35f
m-allanson <michael.allanson@gmail.com>
2021-04-06 14:16:56 +0100

    Add user prompts for title

--- src/write.rs
+++ src/write.rs
@@ -38,6 +38,44 @@ pub fn write(
             })
     });
 
-    dbg!(contents, document_title);
+    let filename = match document_title {
+        Some(raw_title) => confirm_filename(&raw_title),
+        None => ask_for_filename(),
+    };
+    dbg!(contents, filename);
+    
+    fn ask_for_filename() -> Result<String> {
+        rprompt::prompt_reply_stderr(
+            "\
+Enter filename
+>",
+        )
+        .wrap_err("Failed to get filename")
+        .map(|title| slug::slugify(title))
+    }
+
+    fn confirm_filename(raw_title: &str) -> Result<String> {
+        loop {
+            let result = rprompt::prompt_reply_stderr(
+                &format!(
+                    "\
+Current title: `{}`
+Do you want a different title? (y/N): ",
+                    raw_title
+                ),
+            )
+            .wrap_err("Failed to get input for y/b question")?;
+
+            match result.as_str() {
+                "y" | "Y" => break ask_for_filename(),
+                "n" | "N" => break Ok(slug::slugify(raw_title)),
+                _ => {
+                    // ask again because something went wrong
+                }
+            }
+        
+        }
+    }
+
     todo!()
 }
